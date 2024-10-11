034a3934416d3819791bd2f05b90b00eca47e3c2
milindkusahu <milindsahu2000@gmail.com>
2024-10-11 17:33:29 +0530

    Card component updated

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Button from "../../components/Button/Button";
+import Card from "../../components/Card/Card";
 
 const Home = () => {
   return (
     <div>
       <Button />
+      <Card />
     </div>
   );
 };
