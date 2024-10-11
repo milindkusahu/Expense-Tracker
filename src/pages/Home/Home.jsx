aa085396b01de82a8e5cd4a6a25867c4dd9d576a
milindkusahu <milindsahu2000@gmail.com>
2024-10-11 17:35:34 +0530

    BarChart component updated

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Button from "../../components/Button/Button";
 import Card from "../../components/Card/Card";
+import BarChart from "../../components/BarChart/BarChart";
 
 const Home = () => {
   return (
     <div>
       <Button />
       <Card />
+      <BarChart />
     </div>
   );
 };
